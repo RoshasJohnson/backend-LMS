377770cd212b6225f753db44d1646a48b327ceb5
Roshas Johnson <roshasj10gmail.com>
2022-04-27 09:45:37 +0530

    .env created

--- Main/settings.py
+++ Main/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g$hhg&$w=-okj+=tqcxei0e#8%wrg2y8)(%x!=q89+93)m##mc'
+SECRET_KEY = SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,10 +108,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
          'HOST': 'localhost',
-         "NAME":'databook',
         'PORT':'3306' ,
-        'USER': 'root',
-        'PASSWORD':'roshasjohnson!@#$%^&*()_+',
+         "NAME":env("DATABASE NAME"),
+        'USER': env('USER'),
+        'PASSWORD':env('PASSWORD'),
     }
 }
 
---
+++ README.md
@@ -0,0 +1,27 @@
+# Library management system
+
+Django Rest framework Library management system . 
+
+# front is in React js.
+
+React code :
+
+
+
+## how to run
+`mkdir yourfolder` </br>
+`cd yourfolder` </br>
+`virtualenv env`</br> 
+`source env/bin/activate`</br>
+`git clone https://github.com/RoshasJohnson/backend-LMS.git` </br>
+`pip install -r requirements.txt`</br>
+`cd backend-LMS `</br>
+`python manage.py runserver`</br>
+Note: private info are added in .env file so create new .env file in root add them there
+### .env file looks like this
+
+
+`SECRET_KEY=yoursecretkey`</br>
+`database = Your database name`
+`password=yourapppassword`</br>
+`User=your username`</br>
